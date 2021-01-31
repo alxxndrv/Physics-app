864c2305a7998319d91aadda1f129ba549c1570e
alxxndrv <itsalexandrov@gmail.com>
2021-01-31 16:23:52 +0300

    graph for air resistant cases

--- physics/ChartView.swift
+++ physics/ChartView.swift
@@ -10,13 +10,18 @@ import SwiftUICharts
 struct ChartView: View {
     var angle: Int
     var velocity: Double
-    var m: Double
-    var k: Double
+    var m: Double = 0
+    var k: Double = 0
+    var air_resist: Bool
     @State var data: [Double] = [Double]()
     var body: some View {
         VStack {
         LineView(data: data, title: "Высота полета", legend: "измерения в метрах и секундах").onAppear {
+            if air_resist {
             data = get_time_with_air_resistance(angle: angle, velocity: velocity, m: m, k: k).y_list
+            } else {
+                data = get_time(angle: angle, velocity: velocity).y_list
+            }
         }.padding()
         }
     }
